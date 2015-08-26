6f0fa30ae75a8924ada3d0b22eb866609336a63a
Airess <beautebargains@gmail.com>
2015-08-26 01:17:04 -0400

    added validation to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,20 @@ class PostsController < ApplicationController
 	end 
 
 	def new 
+		@post = Post.new
 	end 
 
 	def create 
 		@post = Post.new(post_params)
-		@post.save
 
+		
+		if @post.save
 		redirect_to @post
+
+		else render 'new'
 	end 
+
+end 
 
 	def show 
 		@post = Post.find(params[:id])
@@ -23,3 +29,6 @@ class PostsController < ApplicationController
 		params.require(:post).permit(:title, :body)
 	end
 end
+
+
+
